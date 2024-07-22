80d8b8d4cd008e214ad29bc4d4f23bb6bef3acfb
seonjangwon <wkddnjs740@naver.com>
2024-07-22 11:03:51 +0900

    JWTUtil 추가(네이버 구글 아이디 비번 제외)

---
+++ src/main/java/com/security/oauth2jwt/oauth2/CustomSuccessHandler.java
@@ -0,0 +1,26 @@
+package com.security.oauth2jwt.oauth2;
+
+import com.security.oauth2jwt.jwt.JWTUtil;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+
+@Component
+@RequiredArgsConstructor
+public class CustomSuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
+
+    private final JWTUtil jwtUtil;
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+    }
+
+
+}
